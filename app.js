e7ad4433775f0f45b106f4adbde424631c24d624
adebayo-glitch <pillarbasetech@gmail.com>
2024-08-14 08:31:27 -0400

    class inventory

--- app.js
+++ app.js
@@ -26,3 +26,21 @@ const adventurer = {
   // call roll method
   adventurer.roll();
   adventurer.roll(5);
+
+  
+//   part 2
+
+class Character {
+    constructor(name) {
+      this.name = name;
+      this.health = 100;
+      this.inventory = [];
+    }
+  
+     roll(mod = 0) {
+      const result = Math.floor(Math.random() * 20) + 1 + mod;
+      console.log(`${this.name} rolled a ${result}.`);
+    }
+  }
+  
+  
