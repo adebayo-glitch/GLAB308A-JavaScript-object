010033e4a6112e2497b8ef27f5d4d07bc4468133
adebayo-glitch <pillarbasetech@gmail.com>
2024-08-14 08:34:56 -0400

    new character and call roll

--- app.js
+++ app.js
@@ -43,4 +43,17 @@ class Character {
     }
   }
   
+  const robin = new Character("Robin");
+  robin.inventory = ["sword", "potion", "artifact"];
+  robin.companion = new Character("Leo");
+  robin.companion.type = "Cat";
+  robin.companion.companion = new Character("Frank");
+  robin.companion.companion.type = "Flea";
+  robin.companion.companion.inventory = ["small hat", "sunglasses"];
   
+  // call roll method for Robin and companions
+  robin.roll();
+  robin.companion.roll();
+  robin.companion.companion.roll();
+
+ 
